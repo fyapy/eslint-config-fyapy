455993d9a926397b45e5e81c8410f3ce3b2ba087
fyapy <amirop35@gmail.com>
2021-10-06 14:46:36 +0300

    feature/vue rules

--- packages/eslint-config-fyapy-vue/index.js
+++ packages/eslint-config-fyapy-vue/index.js
@@ -8,4 +8,65 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
+  rules: {
+    'vue/html-indent': ['error', 2],
+    'vue/html-self-closing': 'error',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-quotes': ['error', 'double', { 'avoidEscape': true }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+        allowFirstLine: true,
+      },
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
+    'vue/multiline-html-element-content-newline': 'error',
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/no-multi-spaces': ['error', {
+      ignoreProperties: false,
+    }],
+    'vue/no-spaces-around-equal-signs-in-attribute': ['error'],
+    'vue/one-component-per-file': 'error',
+    'vue/prop-name-casing': ['error', 'snake_case'],
+    'vue/require-prop-types': 'error',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/v-bind-style': ['error', 'shorthand'],
+    'vue/v-on-style': ['error', 'shorthand'],
+    'vue/v-slot-style': ['error', {
+      atComponent: 'shorthand',
+      default: 'shorthand',
+      named: 'shorthand',
+    }],
+    'vue/require-explicit-emits': ['error', {
+      allowProps: false,
+    }],
+    'vue/component-tags-order': ['error', {
+      order: ['script', 'template', 'style'],
+    }],
+    'vue/no-v-html': 'off',
+    'vue/this-in-template': ['error', 'never'],
+    'vue/block-lang': ['error', {
+      script: {
+        lang: 'ts',
+      },
+    }],
+    'vue/no-use-v-if-with-v-for': ['error', {
+      allowUsingIterationVar: false,
+    }],
+    'vue/component-api-style': ['error',
+      ['script-setup', 'composition'], // "script-setup", "composition", or "options"
+    ],
+    'vue/no-static-inline-styles': ['error', {
+      allowBinding: true,
+    }],
+    'vue/no-useless-v-bind': ['error', {
+      ignoreIncludesComment: false,
+      ignoreStringEscape: false,
+    }],
+    'vue/no-useless-template-attributes': 'error',
+    'vue/padding-line-between-blocks': ['error', 'always'],
+  }
 }
